539970b2f5bd49c162b16b80b8081d23c3bc1895
UnTardigrado5g <95749165+UnTardigrado5g@users.noreply.github.com>
2021-12-15 14:31:28 +0100

    install locale dei componenti di node

--- api/index.js
+++
@@ -1,8 +0,0 @@
-var Express = require ("express");
-var app = Express ();
-var fs = require ("fs");
-var cors = require('cors');
-app.use(cors());
-
-var serverboot = document.getElementById('root');
-//module.exports = 'express';
