346bb19fa892e84bb738a737d3abd9cbcae70f2d
Zaffino <46814695+Zaffino@users.noreply.github.com>
2021-12-18 16:20:46 +0100

    Update postMenu.js

--- test/postMenu.js
+++ test/postMenu.js
@@ -5,20 +5,20 @@ const { json, send } = require('express/lib/response');
 
 
 test('POST /menu', (assert) => {
-  jsonToSend = '{"menuType" : "table", "menuList" : [{"piattoID": 0,"name": "vellutata misteriosa","prezzo": 10.50,"image": "../images/logo.png","descrizione": "vellutata della mensa, gg ez sei avvelenato"}]}';
+  jsonToSend = {"menuType" : "table", "menuList" : [{"piattoID": 0,"name": "vellutata misteriosa","prezzo": 10.50,"image": "../images/logo.png","descrizione": "vellutata della mensa, gg ez sei avvelenato"}]};
   request(app)
     .post('/api/menu')
     .send(jsonToSend)
     .set('Accept', 'json')
-    /*.expect(function(res) {
+    .expect(function(res) {
       
       res.body.name = res.body.name;
     })
     .expect(200, {
-      menuID: '0',
-      menuType: 'tipoMenu',
-      menuList: []
-    })*/
+      menuID: 2,
+      menuType: 'table',
+      menuList: [{"piattoID": 0,"name": "vellutata misteriosa","prezzo": 10.50,"image": "../images/logo.png","descrizione": "vellutata della mensa, gg ez sei avvelenato"}]
+    })
     .end((err, res) => {
         console.log(res.body)
         assert.error(err, "no errors")
