4fbde55075fb26060dea601317cf79ef54306491
Zaffino <46814695+Zaffino@users.noreply.github.com>
2021-12-19 12:17:53 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -3,121 +3,12 @@ var request = require('supertest');
 var app = require('../api');
 const { json } = require('express/lib/response');
 
-
-
-// test GET API per prendere il menu
-test('GET menu', (assert) => {
-  request(app)
-      .get('/api/menu')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end((err, res) => {
-          var expectedRes = 
-          {
-            "menu": [
-                {
-                    "menuID": 0,
-                    "menuType": "take away",
-                    "menuList": [
-                        {
-                            "piattoID": 0,
-                            "name": "vellutata misteriosa",
-                            "prezzo": 10.50,
-                            "image": "../images/logo.png",
-                            "descrizione": "vellutata della mensa, gg ez sei avvelenato"
-                        },
-                        {
-                            "piattoID": 1,
-                            "name": "pizza con ananas",
-                            "prezzo": 10.50,
-                            "image": "../images/logo.png",
-                            "descrizione": "non ancora riconosciuta ufficialmente come pizza"
-                        }
-                    ]
-                },
-                {
-                    "menuID": 1,
-                    "menuType": "table",
-                    "menuList": [
-                        {
-                            "piattoID": 0,
-                            "name": "vellutata misteriosa",
-                            "prezzo": 10.50,
-                            "image": "../images/logo.png",
-                            "descrizione": "vellutata della mensa, gg ez sei avvelenato"
-                        },
-                        {
-                            "piattoID": 1,
-                            "name": "pasta al sugo di sputo dello chef",
-                            "prezzo": 10.50,
-                            "image": "../images/logo.png",
-                            "descrizione": "5 stelle sammontana"
-                        },
-                        {
-                            "piattoID": 2,
-                            "name": "pizza",
-                            "prezzo": 10.50,
-                            "image": "../images/logo.png",
-                            "descrizione": "no, non è una pizza bianca"
-                        },
-                        {
-                            "piattoID": 3,
-                            "name": "fenicottero",
-                            "prezzo": 10.50,
-                            "image": "../images/logo.png",
-                            "descrizione": "l'unico piatto rosa a nostra disposizione. Servito freddo."
-                        }
-                    ]
-                }
-            ]
-        }
-
-          //let jsonRes = res.body
-          //console.log(JSON.stringify(res.body))
-          assert.error(err, 'No error');
-          assert.same(res.body, expectedRes, 'res.body as expected');
-          assert.end();
-      });
-      
-});
-
-
-
-
-test('POST /menu', (assert) => {
-      jsonToSend = {"menuType" : "table", "menuList" : [{"piattoID": 0,"name": "vellutata misteriosa","prezzo": 10.50,"image": "../images/logo.png","descrizione": "vellutata della mensa, gg ez sei avvelenato"}]};
-      request(app)
-        .post('/api/menu')
-        .send(jsonToSend)
-        .set('Accept', 'json')
-        .expect(function(res) {
-          
-          res.body.name = res.body.name;
-        })
-        .expect(200, {
-          menuID: 2,
-          menuType: 'table',
-          menuList: [{"piattoID": 0,"name": "vellutata misteriosa","prezzo": 10.50,"image": "../images/logo.png","descrizione": "vellutata della mensa, gg ez sei avvelenato"}]
-        })
-        .end((err, res) => {
-            console.log(res.body)
-            assert.error(err, "no errors")
-        })
-        
-  });
-
-
-
-
-//da capire come funziona il post
 /*
-
-
-test('POST menu' , (assert) => {
-    var postJSONmenu = 'errore ez'; //dovrebbe dare errore
-    request(app).post('/api/menu')
+test('POST piatto' , (assert) => {
+    //var postJSONpiatto = 'errore ez'; //dovrebbe dare errore
+    request(app).post('/api/piatto/:PiattoID')
     .expect('Content-Type', /json/).expect(200)
-    .send(postJSONmenu)
+    //.send(postJSONpiatto)
     .end((err, res) => {
 
         
@@ -127,6 +18,47 @@ test('POST menu' , (assert) => {
         assert.end()
     })
 });
+*/
+
+
+test('GET dipendenti', (assert) => {
+    request(app)
+        .get('/api/dipendenti')
+        //.expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+            
+            assert.error(err, 'No error');
+            //assert.same(res.body, expectedRes, 'res.body as expected');
+            assert.end();
+        });
+        
+  });
+
+
+/*
+
+test('DELETE dipendenti', (assert) => {
+
+    request(app).delete('/api/menu/:MenuID')
+    .send({'MenuID' : 0})
+    .expect(200)
+    .end((err,res) =>{
+        assert.error(err,'no errors')
+        assert.end()
+    })
+})
+
+  */
+
+
+
+
+
+
+
+
+/*
 
 
 
@@ -147,21 +79,6 @@ test('DELETE menu', (assert) => {
 })
 
 
-
-test('POST piatto' , (assert) => {
-    //var postJSONpiatto = 'errore ez'; //dovrebbe dare errore
-    request(app).post('/api/piatto/:PiattoID')
-    .expect('Content-Type', /json/).expect(200)
-    //.send(postJSONpiatto)
-    .end((err, res) => {
-
-        
-        assert.error(err, 'no error')
-
-        //assert.comment(res.body)
-        assert.end()
-    })
-});
 
 //da capire il delete
 test('DELETE piatto', (assert) => {
