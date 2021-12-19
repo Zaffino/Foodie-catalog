73079837c8d29a57bab55a1dd003f2d94a936798
Zaffino <46814695+Zaffino@users.noreply.github.com>
2021-12-19 23:15:38 +0100

    Update deleteMenu.js

--- test/deleteMenu.js
+++ test/deleteMenu.js
@@ -3,15 +3,16 @@ var request = require('supertest');
 var app = require('../api');
 const { json, send } = require('express/lib/response');
 
-test('DELETE /menu', (assert) => {
-  var DELmenuID = {'MenuID' : 0}
-  request(app)
-  .post('/api/menu/:MenuID')
-  .field('MenuID', 0)
+test('DELETE menu', (assert) => {
+  var DELmenuID = {'menuID' : 1};
+  request(app).delete('/api/menu/:MenuID')
+  .send(DELmenuID)
   .expect(200)
   .end((err, res) => {
 
-          
+      
+
+      
       assert.error(err, 'no error')
       assert.end()
   })
